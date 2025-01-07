8d0c6e7617d76b4bcfd75537b1792d7fd0117354
tyler0611 <berlity0611@gmail.com>
2025-01-07 09:54:12 -0600

    Initial Commit

---
+++ Country Flag Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country Flag Game
+//
+//  Created by Tyler Berlin on 1/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
