14cd0208c53057e1fbb9277b95361fceabf69583
tyler0611 <berlity0611@gmail.com>
2025-01-08 09:34:05 -0600

    Created Custom Button

---
+++ Country Flag Game/Components/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  Country Flag Game
+//
+//  Created by Tyler Berlin on 1/8/25.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}
