5d499e3ad4de9947bfd0463ab13c9df61a897d12
tyler0611 <berlity0611@gmail.com>
2025-01-09 14:50:52 -0600

    Created Data and Game Manager

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,37 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Tyler Berlin on 1/8/25.
+//
+
+import Foundation
+
+struct Data {
+    
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+    
+}
+
+
--- Country Flag Game/Models/Question.swift
+++ Country Flag Game/Models/Question.swift
@@ -16,6 +16,6 @@ struct Answer: Identifiable {
 struct Question: Identifiable {
     var id = UUID()
     var correctAnswer: Answer
-    var incorrect: [Answer]
+    var incorrectAnswers: [Answer]
 }
 
